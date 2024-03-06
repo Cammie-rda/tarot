8381c70dfcc5abbcbcf34690ac10e64dbb9c1c60
Cammie-rda <162397904+Cammie-rda@users.noreply.github.com>
2024-03-06 10:14:57 -0300

    Add files via upload

--- tarot.py
+++ tarot.py
@@ -1,19 +1,23 @@
 import random
 
-#implementar opciones del juego
+#Implementar opciones del juego
 arcanos=["El loco", "El mago", "La papisa", "La emperatriz", "El emperador", "El papa", "El enamorado", "El carro", "La justicia", "El ermitaño", "La rueda de la fortuna", "La fuerza", "El colgado", "La muerte", "La templanza", "La diabla", "La torre", "La estrella", "La luna", "El sol", "El juicio", "El mundo"]
 
-#dar Bienvenida
+#Dar Bienvenida
 print("Hola, consultante. Llegaste al tarot!")
-print("No olvides el libre albedrio")
 
-#preguntar al consultante su opción
+#Preguntar al consultante su opción
 opcion_consultante=input(
     "Tienes que barajar:") .capitalize()
+print("Ahora tienes que sacar una carta")
+
+opcion_consultante=input(
+    "Sacar una carta:") .capitalize()
 print("")
 
-#definir la opción del computador
+#Definir la opción del computador
 tarot= random.choice(arcanos)
-print(f"El tarot dice:, '{tarot}'")
+print(f"El tarot dice: {tarot}")
 
+print("No olvides el libre albedrio")
 
